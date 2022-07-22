4834e4a0ecfdef0924a9ae0a4f6cf5ddd91bcccc
Orcdl <ltmt411@gmail.com>
2022-07-22 17:48:03 +0700

    iFrame

---
+++ src/test/java/api_learning/IFrame.java
@@ -0,0 +1,48 @@
+package api_learning;
+
+import driver.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import support.ui.selectEx;
+import url.Urls;
+
+public class IFrame implements Urls {
+
+    public static void main(String[] args) {
+
+        // Get a chrome session
+        WebDriver driver = DriverFactory.getChromeDriver();
+
+        try {
+
+
+            // Locate the iframe
+            By iFrameSel = By.cssSelector("[id$='ifr']");
+            WebElement iFramElem = driver.findElement(iFrameSel);
+
+            // Switch to the iframe
+            driver.switchTo().frame(iFramElem);
+
+            //Locate element inside the iframe
+            WebElement editorInputElem = driver.findElement(By.id("tinymce"));
+            editorInputElem.click();
+            editorInputElem.clear();
+            editorInputElem.sendKeys("This is the new text value....");
+            Thread.sleep(1000);
+
+            // Switch back parent frame
+            driver.switchTo().defaultContent();
+            driver.findElement(By.linkText("Elemental Selenium")).click();
+            Thread.sleep(1000);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    driver.quit();
+
+    }
+
+}
